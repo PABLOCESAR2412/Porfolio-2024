20b8159c31dbdd3c683e5b794411a935c3520975
PABLOCESAR2412 <pablocesar.t.g@gmail.com>
2024-11-23 04:08:17 -0500

    Proyecto-Final-v1.4

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,21 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-    devToolbar: {
-        enabled: false
-    },
+  devToolbar: {
+      enabled: false
+  },
 
-    integrations: [tailwind(), react()],
+  integrations: [tailwind(), react()],
 
-    i18n: {
-        defaultLocale: 'es',
-        locales: ['es', 'en']
-    }
+  i18n: {
+      defaultLocale: 'es',
+      locales: ['es', 'en']
+  },
+
+  output: 'server',
+  adapter: vercel()
 });
